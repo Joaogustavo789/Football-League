80707197e3dd760ef3c8d98d70b89b7278deacf9
Joao Gustavo <jgustavomendonca@gmail.com>
2022-11-23 14:53:25 -0300

    Requisito 18: Criando migration e model de matches

---
+++ app/backend/src/database/models/Matche.model.ts
@@ -0,0 +1,47 @@
+import { Model, INTEGER, BOOLEAN } from 'sequelize';
+import db from '.';
+
+class Matche extends Model {
+  declare id: number;
+  declare homeTeam: number;
+  declare homeTeamGoals: number;
+  declare awayTeam: number;
+  declare awayTeamGoals: number;
+  declare inProgress: boolean;
+}
+
+Matche.init({
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: INTEGER,
+  },
+  homeTeam: {
+    allowNull: false,
+    type: INTEGER,
+  },
+  homeTeamGoals: {
+    allowNull: false,
+    type: INTEGER,
+  },
+  awayTeam: {
+    allowNull: false,
+    type: INTEGER,
+  },
+  awayTeamGoals: {
+    allowNull: false,
+    type: INTEGER,
+  },
+  inProgress: {
+    allowNull: false,
+    type: BOOLEAN,
+  },
+}, {
+  sequelize: db,
+  modelName: 'matches',
+  timestamps: false,
+  underscored: true,
+});
+
+export default Matche;
